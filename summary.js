a4e6649f886f9bba4301a4cf8324285bef3eb53c
theerasak tubrit <theerasak.tubrit@gmail.com>
2021-12-06 14:13:17 +0700

    ok

--- summary.js
+++ summary.js
@@ -3,7 +3,7 @@ var excel = require('excel4node');
 const getOnlineOrders = new Promise((resolve, reject) => {
     var options = {
         'method': 'GET',
-        'url': 'https://api.thamturakit.com/api/orders?channel=%E0%B8%82%E0%B8%B2%E0%B8%A2%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C',
+        'url': 'https://api.thamturakit.com/api/orders?channel=%E0%B8%82%E0%B8%B2%E0%B8%A2%E0%B8%AB%E0%B8%B8%E0%B9%89%E0%B8%99',
         'headers': {
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGUxNmQ4ZDE0OWNmMzAwMTkyYzk1Y2QiLCJmaXJzdE5hbWUiOiJUaGVlcmFzYWsiLCJsYXN0TmFtZSI6IlR1YnJpdCIsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTYzNDg1NjMyNCwiZXhwIjoxNjQyMDU2MzI0fQ.yPvp-t5yJN_Vu1yYCgZOhxB4m7qRAXsZMl4jPujGuhU'
         }
@@ -15,7 +15,7 @@ const getOnlineOrders = new Promise((resolve, reject) => {
         const result = res.data.filter((data) => {
             // return data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'CEREALORDER' && data.payments[0].transactionDateandTime.startsWith('2021-11-20')
             // return data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'CEREALORDER' && ['2021111603012556','2021111603004624','2021111602594698','2021111601254139'].includes(data.id)
-            return data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'CEREALORDER';
+            return data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'SHAREHOLDER' && new Date(data.payments[0].transactionDateandTime) > new Date('2021-06-30');
         })
         resolve(result);
 
@@ -24,43 +24,45 @@ const getOnlineOrders = new Promise((resolve, reject) => {
 
 getOnlineOrders.then((res) => {
     // Create a new instance of a Workbook class
-    // var workbook = new excel.Workbook();
-    // var worksheet = workbook.addWorksheet('Sheet 1');
+    var workbook = new excel.Workbook();
+    var worksheet = workbook.addWorksheet('Sheet 1');
 
 
-    // worksheet.cell(1, 1).string('เลขที่')
-    // worksheet.cell(1, 2).string('วันที่ชำระเงิน')
-    // worksheet.cell(1, 3).string('ชื่อผู้รับ')
-    // worksheet.cell(1, 4).string('เบอร์โทร')
-    // worksheet.cell(1, 5).string('Line name')
-    // worksheet.cell(1, 6).string('ที่อยู่จักส่ง')
-    // worksheet.cell(1, 7).string('จำนวน(กล่อง)')
+    worksheet.cell(1, 1).string('วันที่')
+    worksheet.cell(1, 2).string('จำนวนเงินรวม')
 
 
-    // let i = 2;
-    // res.forEach(order => {
 
-    //     // console.log(order)
 
-    //     worksheet.cell(i, 1).string(order.id)
-    //     worksheet.cell(i, 2).string(`${order.payments[0].transactionDateandTime}`)
-    //     worksheet.cell(i, 3).string(` ${order.shipments[0].title} ${order.shipments[0].firstName} ${order.shipments[0].lastName}`)
-    //     worksheet.cell(i, 4).string(order.shipments[0].tel)
-    //     worksheet.cell(i, 5).string(order.shipments[0].lineName)
-    //     worksheet.cell(i, 6).string(`${order.shipments[0].addr01} ${order.shipments[0].street} ${order.shipments[0].subDistrict} ${order.shipments[0].district} ${order.shipments[0].province} ${order.shipments[0].zip}` )
-    //     worksheet.cell(i, 7).number(order.items[0].qty)
 
-    //     i++;
-    // });
+    var result = [];
+    res.reduce(function (res, value) {
+        if (!res[value.payments[0].transactionDateandTime.substring(0, 10)]) {
+            res[value.payments[0].transactionDateandTime.substring(0, 10)] = { trnsDate: value.payments[0].transactionDateandTime.substring(0, 10), amount: value.netAmount }
 
-    // workbook.write('รายการจัดส่งซีเรียสซีเรียล-20211120.xlsx');
-    let total = 0;
-    res.forEach(order => {
-        console.log(order.payments[0].transactionDateandTime);
-        total += order.items[0].qty;
+            result.push(res[value.payments[0].transactionDateandTime.substring(0, 10)])
+        }
+
+        res[value.payments[0].transactionDateandTime.substring(0, 10)].amount += value.netAmount;
+        return res;
+    }, {});
+
+    console.log(result);
+
+    let i = 2;
+    result.forEach(order => {
+
+        console.log(order)
+
+        worksheet.cell(i, 1).string(`${order.trnsDate}`)
+        worksheet.cell(i, 2).string(`${order.amount}`)
+
+        i++;
     });
 
-    console.log(total);
+    workbook.write(`สรุปยอดรวมซื้อหุ้นรายวัน.xlsx`);
+
+    // console.log(total);
 })
 
 
