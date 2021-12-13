0faec63e0f69ba381e693104486a2d406a5b5ca3
theerasak tubrit <theerasak.tubrit@gmail.com>
2021-12-13 14:32:05 +0700

    ok

--- campaign.js
+++ campaign.js
@@ -13,7 +13,8 @@ const getOnlineOrders = new Promise((resolve, reject) => {
 
         const res = JSON.parse(response.body);
         const result = res.data.filter((data) => {
-            return data.items[0].qty >= 100 && data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'SHAREHOLDER' && new Date(data.payments[0].transactionDateandTime) > new Date('2021-10-01') && new Date(data.payments[0].transactionDateandTime) < new Date('2021-12-06');
+            // return data.items[0].qty >= 100 && data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'SHAREHOLDER' && new Date(data.payments[0].transactionDateandTime) > new Date('2021-10-01') && new Date(data.payments[0].transactionDateandTime) < new Date('2021-12-06');
+            return data.payments.length > 0 && data.payments[0].billPaymentRef2 === 'SHAREHOLDER' && new Date(data.payments[0].transactionDateandTime) < new Date('2021-11-01');
         })
         resolve(result);
 
@@ -37,36 +38,55 @@ const getAllContacts = new Promise((resolve, reject) => {
 })
 
 getOnlineOrders.then((res) => {
-    getAllContacts.then((contacts) => {
-        // Create a new instance of a Workbook class
-        var workbook = new excel.Workbook();
-        var worksheet = workbook.addWorksheet('Sheet 1');
+    // getAllContacts.then((contacts) => {
+    //     // Create a new instance of a Workbook class
+    //     var workbook = new excel.Workbook();
+    //     var worksheet = workbook.addWorksheet('Sheet 1');
 
 
-        worksheet.cell(1, 1).string('เลขที่')
-        worksheet.cell(1, 2).string('วันที่ชำระเงิน')
-        worksheet.cell(1, 3).string('ชื่อผู้รับ')
-        worksheet.cell(1, 4).string('เบอร์โทร')
-        worksheet.cell(1, 5).string('Line name')
-        worksheet.cell(1, 6).string('ที่อยู่จักส่ง')
-        worksheet.cell(1, 7).string('จำนวน(หุ้น)')
+    //     worksheet.cell(1, 1).string('เลขที่')
+    //     worksheet.cell(1, 2).string('วันที่ชำระเงิน')
+    //     worksheet.cell(1, 3).string('ชื่อผู้ถือหุ้น')
+    //     worksheet.cell(1, 4).string('มูลค่าหุ้น(บาท)')
 
-        let i = 2;
-        res.forEach(order => {
+    //     let i = 2;
+    //     res.forEach(order => {
 
-            // console.log(order)
-            const xx = contacts.find(ct => ct._id === order.customerId);
-            worksheet.cell(i, 1).string(order.id)
-            worksheet.cell(i, 2).string(`${order.payments[0].transactionDateandTime}`)
-            worksheet.cell(i, 3).string(` ${xx.title} ${xx.firstName} ${xx.lastName}`)
-            worksheet.cell(i, 4).string(xx.tel)
-            worksheet.cell(i, 5).string(xx.lineName)
-            worksheet.cell(i, 6).string(`${xx.addr01} ${xx.street} ${xx.subDistrict} ${xx.district} ${xx.province} ${xx.zip}`)
-            worksheet.cell(i, 7).number(order.items[0].qty)
+    //         console.log(order)
+    //         // const xx = contacts.find(ct => ct._id === order.customerId);
+    //         worksheet.cell(i, 1).string(order.id)
+    //         worksheet.cell(i, 2).string(`${order.payments[0].transactionDateandTime}`)
+    //         worksheet.cell(i, 3).string(` ${order.customerName}`)
+    //         worksheet.cell(i, 4).number(order.items[0].netAmount)
 
-            i++;
-        });
+    //         i++;
+    //     });
 
-        workbook.write(`รายการจัดส่งาวหอมมะลิธรรมชาติแท้ใหม่ต้นฤดูที่ซื้อหุ้นตั้งแต่100หุ้นขึ้นไป.xlsx`);
+    //     workbook.write(`รายงานซื้อหุ้นถึงวันที่-31-10-2564.xlsx`);
+    // });
+
+
+    var workbook = new excel.Workbook();
+    var worksheet = workbook.addWorksheet('Sheet 1');
+
+
+    worksheet.cell(1, 1).string('เลขที่')
+    worksheet.cell(1, 2).string('วันที่ชำระเงิน')
+    worksheet.cell(1, 3).string('ชื่อผู้ถือหุ้น')
+    worksheet.cell(1, 4).string('มูลค่าหุ้น(บาท)')
+
+    let i = 2;
+    res.forEach(order => {
+
+        //console.log(order)
+        // const xx = contacts.find(ct => ct._id === order.customerId);
+        worksheet.cell(i, 1).string(order.id)
+        worksheet.cell(i, 2).string(`${order.payments[0].transactionDateandTime}`)
+        worksheet.cell(i, 3).string(` ${order.customerName}`)
+        worksheet.cell(i, 4).number(order.items[0].netAmount)
+
+        i++;
     });
+
+    workbook.write(`รายงานซื้อหุ้นถึงวันที่-31-10-2564.xlsx`);
 })
